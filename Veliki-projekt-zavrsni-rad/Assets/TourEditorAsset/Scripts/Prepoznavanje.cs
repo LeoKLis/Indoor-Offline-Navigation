b9664ab553dd7831a821ac30e464c983dbedfbc4
FrancescoGlasnovic <64422825+FrancescoGlasnovic@users.noreply.github.com>
2022-05-17 03:31:05 +0200

    dela

--- Veliki-projekt-zavrsni-rad/Assets/TourEditorAsset/Scripts/Prepoznavanje.cs
+++ Veliki-projekt-zavrsni-rad/Assets/TourEditorAsset/Scripts/Prepoznavanje.cs
@@ -9,7 +9,7 @@ public class Prepoznavanje : MonoBehaviour
     public GameObject[] Buttons;
     public Graph myGraph;
     int index;
-    
+    public Material materijal;
     void Start()
     {
         Scene scena = SceneManager.GetActiveScene();
@@ -18,13 +18,14 @@ public class Prepoznavanje : MonoBehaviour
         for (int i = 0; i < myGraph.nodes.Count; i++)
         {
             //Debug.Log(myGraph.nodes[i].ToString() + " || " + scena + " (Node)");
-            if (myGraph.nodes[i].ToString() == scena + " (Node)")
+            if (myGraph.nodes[i].ToString() == scena.name + " (Node)")
             {
                 index = i;
             }
         }
         Path path = myGraph.GetShortestPath(myGraph.nodes[index], myGraph.nodes[30]);
         int d = 0;
+        Debug.Log("scena" + scena.name);
         foreach (GameObject i in Buttons)
         {
 
@@ -33,7 +34,10 @@ public class Prepoznavanje : MonoBehaviour
             Debug.Log(pomicanje.scena + " ||| " + path.nodes[1].ToString());
             if(pomicanje.scena+" (Node)"==path.nodes[1].ToString())
             {
-                Debug.Log(Buttons[d]+" daj bože");
+                Debug.Log(Buttons[d] + " daj bože");
+                Buttons[d].GetComponent<MeshRenderer>().material=materijal;
+                
+                
             }
             
 
